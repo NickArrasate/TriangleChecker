05b076e60065e231c7805ef163d2fb05a44c654d
blake bless and nick arrasate <blake@>
2016-03-24 14:07:01 -0700

    make page that determines quality of  triangles

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
 
   $("form#inputs").submit(function(event) {
 
-  var side1 = $("#side1").val();
-  var side2 = $("#side2").val();
-  var side3 = $("#side3").val();
+  var side1 = parseInt($("#side1").val());
+  var side2 = parseInt($("#side2").val());
+  var side3 = parseInt($("#side3").val());
 
   if (side1 === side2 && side1 === side3){
     $(".equilateral").show();
@@ -14,6 +14,9 @@ $(document).ready(function() {
   else if (side1 != side2 && side2 != side3 && side1 != side3){
       $(".scalene").show();
     }
+  else if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1){
+    alert("that's not a triangle, fool")
+  }
   else {
     $(".isosceles").show();
   }
